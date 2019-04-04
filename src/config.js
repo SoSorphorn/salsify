f4c5a6dcff3749a4e209b5ff0070f2c0b40d9337
sorphorn <so.sorphorn@gmail.com>
2019-04-04 17:00:25 +0700

    add config with amplify in project

---
+++ src/config.js
@@ -0,0 +1,20 @@
+export default {
+    s3: {
+      REGION: "us-east-1", 
+      BUCKET: "salsify-app"
+    }, 
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://4f38laiqx3.execute-api.us-east-1.amazonaws.com/prod" 
+    },
+      cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_ofxutAZaA",
+      APP_CLIENT_ID: "3sn50db9u9ani9kj9l3jnbfnop",
+      IDENTITY_POOL_ID: "us-east-1:42c439e9-a3db-4d6e-9431-e0d799bb6425" 
+    },
+  };
+  
+  
+  
+  
--- src/index.js
+++ src/index.js
@@ -4,7 +4,31 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import config from './config.js'
 
+
+Amplify.configure({ 
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID, 
+    identityPoolId: config.cognito.IDENTITY_POOL_ID, 
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  }, 
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  }, 
+  API: {
+    endpoints: [ {
+      name: "salsify",
+      endpoint: config.apiGateway.URL, 
+      region: config.apiGateway.REGION
+    }]
+  }
+});
 
 ReactDOM.render(
   <Router><App /></Router>, 
